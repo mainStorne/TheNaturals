3036d0a296c44b5729f537136d2413dd52c191eb
mainStorne <dbevzpr@mail.ru>
2023-09-03 16:16:03 +0200

    Pre Alpha 0.0.1

---
+++ constants.py
@@ -0,0 +1,6 @@
+# size of screen
+size = [800, 600]
+FPS = 10
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (50, 82, 123)
--- main.py
+++ main.py
@@ -1,59 +1,10 @@
-# hello player
-
-# import os
-# import time
-#
-#
-# def delay(text: str, t=0.25):
-#     for char in text:
-#         print(char, end='', flush=True)
-#         time.sleep(t)
-#
-#
-# welcome_text = ("Hello player! You will play soon!\n"
-#                 "Please write your name: ")
-#
-# delay(welcome_text, 0.05)
-# name_player = input()
-# delay(name_player)
-# os.system('cls')
-# print('Success! Welcome to this World!')
-
-# def foo(*args):
-#     for arg in args:
-#         print("arg is", arg)
-
-# myTuple = (1, 1.2, "st", True)
-#
-# print(type(myTuple), *myTuple)
-# foo(myTuple)
-
-# myList = [2, 2.3, "on", False]
-#
-# print(type(myList), *myList)
-#
-# foo(myList)
-# myList.copy()
-#
-# print(myList)
-
-#myDict = {3: "hello", "st": True}
-
-#mySet = {3, 3.4, "e", True, False}
-
 import pygame
+import constants
 # initialization pygame mode
 pygame.init()
-# size of screen
-size = [800, 600]
-FPS = 10
 # This our surface
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(constants.size)
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-BLUE = (50, 82, 123)
-#pygame.display.пу
 pygame.display.set_caption("Game of TheNaturals  Pre-Alpha")
 # flag of the circle
 done = False
@@ -72,24 +23,19 @@ def move(start) -> list:
     else:
         return start
 
-import random
-
 
 start_pos = [400, 200]
-print("start", start_pos)
 while not done:
-    clock.tick(FPS)
-    print(start_pos)
-    for event in pygame.event.get():
+    clock.tick(constants.FPS)
 
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
 
-    screen.fill(BLACK)
+    screen.fill(constants.BLACK)
 
     move(start_pos)
-    pygame.draw.circle(screen, WHITE, start_pos, 14)
-
+    pygame.draw.circle(screen, constants.WHITE, start_pos, 14)
     pygame.display.flip()
 
 pygame.quit()
